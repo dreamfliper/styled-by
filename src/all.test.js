d3dc30493c523a6d1d5d0d9f69841194ba9374c9
Henpai Hsu <dreamfliper@gmail.com>
2019-09-10 18:21:45 +0800

    feat: support nest path in every resolving prop

--- src/all.test.js
+++ src/all.test.js
@@ -1,130 +1,148 @@
-import test from 'ava'
-import styledBy from '.'
+import test from "ava";
+import styledBy from ".";
 
 const props = {
 	margin: true,
-	padding: '10px',
-	size: 'small',
-	corner: 'rounded',
-	blocked: true
-}
-
-test('passed props', t => {
-	const result = styledBy('padding')(props)
-	t.is(result, '10px')
-})
-
-test('string', t => {
-	const padding = styledBy('padding', '15px')(props)
-	const margin = styledBy('margin', '25px')(props)
-
-	t.is(padding, '15px')
-	t.is(margin, '25px')
-})
-
-test('function', t => {
-	const padding = styledBy('padding', props => props.padding)(props)
-
-	t.is(padding, '10px')
-})
-
-test('object with string', t => {
-	const options = {
-		small: 'font-size: 0.8rem;',
-		medium: 'font-size: 1rem;',
-		large: 'font-size: 1.2rem;'
+	padding: "10px",
+	size: "small",
+	corner: "rounded",
+	blocked: true,
+	inner: {
+		mode: "hight"
 	}
+};
 
-	const size = styledBy('size', options)(props)
+test("passed props", t => {
+	const result = styledBy("padding")(props);
+	t.is(result, "10px");
+});
 
-	t.is(size, options[props.size])
-})
+test("string", t => {
+	const padding = styledBy("padding", "15px")(props);
+	const margin = styledBy("margin", "25px")(props);
 
-test('object with string and no match', t => {
+	t.is(padding, "15px");
+	t.is(margin, "25px");
+});
+
+test("function", t => {
+	const padding = styledBy("padding", props => props.padding)(props);
+
+	t.is(padding, "10px");
+});
+
+test("object with string", t => {
+	const options = {
+		small: "font-size: 0.8rem;",
+		medium: "font-size: 1rem;",
+		large: "font-size: 1.2rem;"
+	};
+
+	const size = styledBy("size", options)(props);
+
+	t.is(size, options[props.size]);
+});
+
+test("object with string and no match", t => {
 	const options = {
 		_: props => `font-size: ${props.size};`,
-		small: 'font-size: 0.8rem;',
-		medium: 'font-size: 1rem;',
-		large: 'font-size: 1.2rem;'
-	}
+		small: "font-size: 0.8rem;",
+		medium: "font-size: 1rem;",
+		large: "font-size: 1.2rem;"
+	};
 
-	const size = styledBy('size', options)({ size: '0.5rem' })
+	const size = styledBy("size", options)({ size: "0.5rem" });
 
-	t.is(size, 'font-size: 0.5rem;')
-})
+	t.is(size, "font-size: 0.5rem;");
+});
 
-test('object with function', t => {
+test("object with function", t => {
 	const options = {
-		default: props => (props.blocked ? 'default blocked' : 'default unblocked'),
-		rounded: props => (props.blocked ? 'rounded blocked' : 'rounded unblocked'),
-		circle: props => (props.blocked ? 'circle blocked' : 'circle unblocked')
-	}
+		default: props => (props.blocked ? "default blocked" : "default unblocked"),
+		rounded: props => (props.blocked ? "rounded blocked" : "rounded unblocked"),
+		circle: props => (props.blocked ? "circle blocked" : "circle unblocked")
+	};
 
-	const corner = styledBy('corner', options)(props)
+	const corner = styledBy("corner", options)(props);
 
-	t.is(corner, options[props.corner](props))
-})
+	t.is(corner, options[props.corner](props));
+});
 
-test('object with options', t => {
+test("object with options", t => {
 	const options = {
 		size: {
-			small: 'font-size: 0.8rem;',
-			medium: 'font-size: 1rem;',
-			large: 'font-size: 1.2rem;'
+			small: "font-size: 0.8rem;",
+			medium: "font-size: 1rem;",
+			large: "font-size: 1.2rem;"
 		}
-	}
+	};
 
-	const size = styledBy(options)(props)
+	const size = styledBy(options)(props);
 
-	t.is(size, ` ${options.size[props.size]}`)
-})
+	t.is(size, ` ${options.size[props.size]}`);
+});
 
-test('object with many options', t => {
+test("object with many options", t => {
 	const options = {
 		size: {
-			small: 'font-size: 0.8rem;',
-			medium: 'font-size: 1rem;',
-			large: 'font-size: 1.2rem;'
+			small: "font-size: 0.8rem;",
+			medium: "font-size: 1rem;",
+			large: "font-size: 1.2rem;"
 		},
 		corner: {
-			square: 'border-radius: 0;',
-			rounded: 'border-radius: 5px;'
+			square: "border-radius: 0;",
+			rounded: "border-radius: 5px;"
 		}
-	}
+	};
 
-	const size = styledBy(options)(props)
+	const size = styledBy(options)(props);
 
-	t.is(size, ` ${options.size[props.size]} ${options.corner[props.corner]}`)
-})
+	t.is(size, ` ${options.size[props.size]} ${options.corner[props.corner]}`);
+});
 
-test('object with options with defaults', t => {
+test("object with options with defaults", t => {
 	const options = {
 		size: {
-			small: 'font-size: 0.8rem;',
-			medium: 'font-size: 1rem;',
-			large: 'font-size: 1.2rem;'
+			small: "font-size: 0.8rem;",
+			medium: "font-size: 1rem;",
+			large: "font-size: 1.2rem;"
 		}
-	}
+	};
 
-	const size = styledBy(options, { size: 'large' })({})
+	const size = styledBy(options, { size: "large" })({});
 
-	t.is(size, ` ${options.size.large}`)
-})
+	t.is(size, ` ${options.size.large}`);
+});
 
-test('object with many options with defaults', t => {
+test("object with many options with defaults", t => {
 	const options = {
 		size: {
-			small: 'font-size: 0.8rem;',
-			medium: 'font-size: 1rem;',
-			large: 'font-size: 1.2rem;'
+			small: "font-size: 0.8rem;",
+			medium: "font-size: 1rem;",
+			large: "font-size: 1.2rem;"
 		},
 		corner: {
-			square: 'border-radius: 0;',
-			rounded: 'border-radius: 5px;'
+			square: "border-radius: 0;",
+			rounded: "border-radius: 5px;"
 		}
-	}
+	};
 
-	const size = styledBy(options, { size: 'large' })({ corner: 'rounded' })
+	const size = styledBy(options, { size: "large" })({ corner: "rounded" });
 
-	t.is(size, ` ${options.size.large} ${options.corner.rounded}`)
-})
+	t.is(size, ` ${options.size.large} ${options.corner.rounded}`);
+});
+
+test("nest props", t => {
+	const result = styledBy("inner.mode")(props);
+	t.is(result, "hight");
+});
+
+test("nest props with object option", t => {
+	const options = {
+		hight: "hit"
+	};
+
+	const result = styledBy("inner.mode", options)(props);
+
+	t.is(result, "hit");
+});
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const path = (obj, address) =>
 	address.reduce((result, key) => result && result[key], obj);
 
 const mapOpt = {
-	function: ({ options, props, prop }) => options[props[prop]](props),
-	string: ({ options, props, prop }) => options[props[prop]],
+	function: ({ options, props, prop }) =>
+		options[path(props, prop.split("."))](props),
+	string: ({ options, props, prop }) => options[path(props, prop.split("."))],
 	undefined: ({ options, props }) =>
 		typeof options._ === "function" ? options._(props) : null
 };
@@ -12,7 +13,11 @@ const mapOptions = {
 	string: ({ options }) => options,
 	function: ({ options, props }) => options(props),
 	object: ({ options, props, prop }) =>
-		mapOpt[typeof options[props[prop]]]({ options, props, prop }),
+		mapOpt[typeof options[path(props, prop.split("."))]]({
+			options,
+			props,
+			prop
+		}),
 	undefined: () => {}
 };
 
